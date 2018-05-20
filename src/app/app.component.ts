e9d9323c14a2fa3f2ef7883859dc17fb687847cf
nrocchi <nrocchi@gmail.com>
2018-05-20 15:24:17 +0200

    oc-angular-activite1 First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  posts = [
+    new Post('Premier post', 'Ceci est le contenu du premier post'),
+    new Post('Deuxième post', 'Ceci est le contenu du deuxième post'),
+    new Post('Troisième post', 'Ceci est le contenu du troisième post')
+  ];
+
 }
